64a0775ad4e11dfbf5605dbe10d455b55e23cd91
Jukka Svahn <web@rahinaa.biz>
2014-03-29 00:15:14 +0200

    Indent with spaces.

--- rah_maintenance.php
+++ rah_maintenance.php
@@ -14,49 +14,49 @@
 
 class rah_maintenance
 {
-	/**
-	 * Constructor.
-	 */
+    /**
+     * Constructor.
+     */
 
-	public function __construct()
-	{
-		register_callback(array($this, 'install'), 'plugin_lifecycle.rah_maintenance', 'installed');
-		register_callback(array($this, 'uninstall'), 'plugin_lifecycle.rah_maintenance', 'deleted');
-		register_callback(array($this, 'error_page'), 'pretext');
-	}
+    public function __construct()
+    {
+        register_callback(array($this, 'install'), 'plugin_lifecycle.rah_maintenance', 'installed');
+        register_callback(array($this, 'uninstall'), 'plugin_lifecycle.rah_maintenance', 'deleted');
+        register_callback(array($this, 'error_page'), 'pretext');
+    }
 
-	/**
-	 * Installer.
-	 */
+    /**
+     * Installer.
+     */
 
-	public function install()
-	{
-		if (get_pref('rah_maintenance_active', false) === false)
-		{
-			set_pref('rah_maintenance_active', 0, 'site', PREF_PLUGIN, 'yesnoradio', 81);
-		}
-	}
+    public function install()
+    {
+        if (get_pref('rah_maintenance_active', false) === false)
+        {
+            set_pref('rah_maintenance_active', 0, 'site', PREF_PLUGIN, 'yesnoradio', 81);
+        }
+    }
 
-	/**
-	 * Uninstaller.
-	 */
+    /**
+     * Uninstaller.
+     */
 
-	public function uninstall()
-	{
-		safe_delete('txp_prefs', "name like 'rah\_maintenance\_%'");
-	}
+    public function uninstall()
+    {
+        safe_delete('txp_prefs', "name like 'rah\_maintenance\_%'");
+    }
 
-	/**
-	 * Display error page when maintenance mode is active.
-	 */
+    /**
+     * Display error page when maintenance mode is active.
+     */
 
-	public function error_page()
-	{
-		if (get_pref('rah_maintenance_active') && !is_logged_in())
-		{
-			txp_die(gTxt('rah_maintenance_in_progress'), 503);
-		}
-	}
+    public function error_page()
+    {
+        if (get_pref('rah_maintenance_active') && !is_logged_in())
+        {
+            txp_die(gTxt('rah_maintenance_in_progress'), 503);
+        }
+    }
 }
 
 new rah_maintenance();
